d6e82123aa790502a220491bfefa5423cc18a068
ummjevel <mseagle2023@gmail.com>
2023-02-15 12:54:17 +0900

    add description button and modal

--- web/flask/static/js/anichat.js
+++ web/flask/static/js/anichat.js
@@ -22,6 +22,64 @@ var hiddenBalloon;
 
 $(document).ready(function () {
 
+    // slider
+    var currentIndex = 0,
+      navItems = $('.nav li');
+  
+    function setSlide(index) {
+        navItems.removeClass('selected');
+        navItems.eq(index).addClass('selected');
+        $('.slide').css('display', 'none');
+        $('.slide').eq(index).css('display', 'block');
+    }
+    
+    $('.nav li').click(function() {
+        var index = $('.nav li').index($(this));
+        currentIndex = index;
+        setSlide(currentIndex);
+    });
+    
+    function next() {
+        if (currentIndex < (navItems.length - 1)) {
+        currentIndex++;
+        setSlide(currentIndex);
+        }
+    }
+    
+    $('.next').click(function() {
+        next();
+    });
+    
+    function prev() {
+        if (currentIndex > 0) {
+        currentIndex--;
+        setSlide(currentIndex);
+        }
+    }
+    
+    $('.prev').click(function() {
+        prev();
+    });
+    
+    document.addEventListener('keydown', function(event) {
+        if (event.keyCode == 39) {
+        next();
+        } else if (event.keyCode == 37){
+        prev();
+        }
+    }, true);
+    
+    function slide() {
+        if (currentIndex < (navItems.length - 1)) {
+        currentIndex++;
+        setSlide(currentIndex);
+        } else {
+        currentIndex = 0;
+        setSlide(currentIndex);
+        }
+    }
+
+    // modal
     var modal = document.getElementById("modalCharacter");
     var modalRecorder = document.getElementById("modalRecorder");
     var modalHelper = document.getElementById("modalHelper");
@@ -286,7 +344,16 @@ $(document).ready(function () {
         });
         
     });
+
+    $("#imgHelper").click(function() {
+        $("#modalHelper").css('display', 'block');
+    });
+
 });
+
+function closeHelperModal() {
+    $("#modalHelper").css('display', 'none');
+}
 
 function validationCheck(message) {
     // 이상한 문자 검열하기.
